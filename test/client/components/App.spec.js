f213ca876f43037bcc536f1bb8927149b81c305d
Zack Kollar (SeedyROM) <rallokkcaz@gmail.com>
2018-04-04 02:23:09 -0700

    Working on client testing CI issues

--- test/client/components/App.spec.js
+++ test/client/components/App.spec.js
@@ -4,15 +4,10 @@ import { expect } from 'chai'
 
 import App from '../../../src/client/components/App'
 
-require('../../helpers/async-dump')()
-
 describe('<App/>', () => {
   it('should have an h1', () => {
     const wrapper = shallow(<App/>)
     expect(wrapper.find('h1')).to.have.length(1)
-  })
-  after(function () {
-    global.asyncDump()
   })
 })
 
