529717558c086602fe569608281ea48119fd37a0
Zack Kollar (SeedyROM) <rallokkcaz@gmail.com>
2018-04-04 02:36:52 -0700

    Fixed with brute hack, will do for now

--- test/helpers/browser.js
+++ test/helpers/browser.js
@@ -13,11 +13,20 @@ global.document = document
 global.window = document.defaultView
 Object.keys(document.defaultView).forEach((property) => {
     if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property)
-    global[property] = document.defaultView[property]
+        exposedProperties.push(property)
+        global[property] = document.defaultView[property]
     }
 })
 
 global.navigator = {
     userAgent: 'node.js'
 }
+
+//
+// PROBLEM: Clientside tests never exit regardless of the mocha configuration.
+// TEMP PATCH: Kill the process at the end of the testsuite.
+//
+// TODO: Find the fix for this hack!
+after(() => {
+    process.exit(0)
+})
